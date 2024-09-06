1d777604b381ba7dc4227bf0d6b9edfe1bc792a6
zaibjeeofficial <muhammadfaizraza48@gmail.com>
2024-09-07 01:18:48 +0500

    cors issue resolve

--- app.js
+++ app.js
@@ -2,12 +2,19 @@ const express = require("express");
 const app = express();
 const errorMiddleware = require("./middleware/error");
 const cookieParser = require("cookie-parser");
-var session = require("express-session");
+const session = require("express-session");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
 app.use(express.json());
 app.use(cookieParser("cookie"));
 
-
-
+app.use(
+  cors({
+    origin: "*",
+    credentials: true,
+  })
+);
 
 app.use(
   session({
@@ -17,22 +24,24 @@ app.use(
   })
 );
 
-const user = require("./route/userRoute.js");
-const score = require("./route/scoreRoute.js");
-const bodyParser = require("body-parser");
 app.use(
   session({
     secret: "abcdefg",
     resave: true,
     saveUninitialized: false,
-    cookie: { secure: true }, // this line
+    cookie: { secure: true },
   })
 );
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+const user = require("./route/userRoute.js");
+const score = require("./route/scoreRoute.js");
+
 app.use("/api/v1", user);
 app.use("/api/v1", score);
-//middleware
+
 app.use(errorMiddleware);
 
 module.exports = app;
