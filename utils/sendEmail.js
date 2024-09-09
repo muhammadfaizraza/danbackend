d085f418e74ab2ba236fefc7e7f00f8faa5528ea
muhammad faiz raza <muhammad.faiz@avancerasolutions.com>
2024-09-09 16:35:34 +0500

    env update

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,22 +1,30 @@
-const nodeMailer = require("nodemailer");
+const nodemailer = require("nodemailer");
 
 const sendEmail = async (option) => {
-  const transporter = nodeMailer.createTransport({
-    host: process.env.SMPT_HOST,
-    port: 465,
-    service: process.env.SMPT_SERVICE,
+  const transporter = nodemailer.createTransport({
+    host: "smtp.verio.com", // SMTP server host
+    port: 465, // Port (587 for TLS)
+    secure: true, // Use TLS
     auth: {
-      user: process.env.SMPT_MAIL,
-      pass: process.env.SMPT_PASSWORD,
+      user: "letsplay@ipropaganda.com", // Your Verio email address
+      pass: "Spring2023@April", // Your Verio email password
     },
+    debug: true, // Enable debug output
   });
-  const mailOption = {
-    from: process.env.SMPT_MAIL,
+
+  const mailOptions = {
+    from: "letsplay@ipropaganda.com",
     to: option.email,
     subject: option.subject,
     text: option.message,
   };
-  await transporter.sendMail(mailOption);
+
+  try {
+    await transporter.sendMail(mailOptions);
+    console.log("Email sent successfully");
+  } catch (error) {
+    console.error("Error sending email:", error);
+  }
 };
 
 module.exports = sendEmail;
